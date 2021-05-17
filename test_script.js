21d082e9e895baf20fcf97f6b1bdd1b627f17d90
Felps-95 <84329276+Felps-95@users.noreply.github.com>
2021-05-17 16:48:10 -0300

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -3,5 +3,3 @@
             var color = 'rgba('.concat(Math.random()*256,',',Math.random()*256,',',Math.random()*256,',',Math.random(),')')
             elem.style.color = color;
         }
-
-document.getElementById('NavSet').style.margin = '0px -10px';
